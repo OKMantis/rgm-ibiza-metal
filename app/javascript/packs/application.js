06d227914826c3122cba0e785d02d2d32d087d0e
Owen Moss <owenkmoss@gmail.com>
2021-05-03 20:02:21 +0200

    setting up owl-carousel (not finished)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,8 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -27,4 +29,8 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+});
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
 });
