4c8f6275d37f25d1bfcf70a040083bfe821eca2b
Owen Moss <owenkmoss@gmail.com>
2021-05-04 13:39:47 +0200

    isotope not working yet

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1351,39 +1351,39 @@ function windowLoadInit() {
 	}
 
 	// init Isotope
-	// $('.isotope-wrapper').each(function(index) {
-	// 	var $container = $(this);
-	// 	var layoutMode = ($container.hasClass('masonry-layout')) ? 'masonry' : 'fitRows';
-	// 	var columnWidth = ($container.children('.col-md-4').length) ? '.col-md-4' : false;
-	// 	$container.isotope({
-	// 		percentPosition: true,
-	// 		layoutMode: layoutMode,
-	// 		masonry: {
-	// 			//TODO for big first element in grid - giving smaller element to use as grid
-	// 			// columnWidth: '.isotope-wrapper > .col-md-4'
-	// 			columnWidth: columnWidth
-	// 		}
-	// 	});
+	$('.isotope-wrapper').each(function(index) {
+		var $container = $(this);
+		var layoutMode = ($container.hasClass('masonry-layout')) ? 'masonry' : 'fitRows';
+		var columnWidth = ($container.children('.col-md-4').length) ? '.col-md-4' : false;
+		$container.isotope({
+			percentPosition: true,
+			layoutMode: layoutMode,
+			masonry: {
+				//TODO for big first element in grid - giving smaller element to use as grid
+				// columnWidth: '.isotope-wrapper > .col-md-4'
+				columnWidth: columnWidth
+			}
+		});
 
-	// 	var $filters = $container.attr('data-filters') ? $($container.attr('data-filters')) : $container.prev().find('.filters');
-	// 	// bind filter click
-	// 	if ($filters.length) {
-	// 		$filters.on( 'click', 'a', function( e ) {
-	// 			e.preventDefault();
-	// 			var $thisA = $(this);
-	// 			var filterValue = $thisA.attr('data-filter');
-	// 			$container.isotope({ filter: filterValue });
-	// 			$thisA.siblings().removeClass('selected active');
-	// 			$thisA.addClass('selected active');
-	// 		});
-	// 		//for works on select
-	// 		$filters.on( 'change', 'select', function( e ) {
-	// 			e.preventDefault();
-	// 			var filterValue = $(this).val();
-	// 			$container.isotope({ filter: filterValue });
-	// 		});
-	// 	}
-	// });
+		var $filters = $container.attr('data-filters') ? $($container.attr('data-filters')) : $container.prev().find('.filters');
+		// bind filter click
+		if ($filters.length) {
+			$filters.on( 'click', 'a', function( e ) {
+				e.preventDefault();
+				var $thisA = $(this);
+				var filterValue = $thisA.attr('data-filter');
+				$container.isotope({ filter: filterValue });
+				$thisA.siblings().removeClass('selected active');
+				$thisA.addClass('selected active');
+			});
+			//for works on select
+			$filters.on( 'change', 'select', function( e ) {
+				e.preventDefault();
+				var filterValue = $(this).val();
+				$container.isotope({ filter: filterValue });
+			});
+		}
+	});
 
 
 	/////////
