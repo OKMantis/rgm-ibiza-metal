6f9f1818fcfdec6fdbb3cb6607b7b332f96b0ca7
Owen Moss <owenkmoss@gmail.com>
2021-05-04 12:06:48 +0200

    theme without the js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,8 +20,6 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -29,8 +27,4 @@ import 'owl.carousel';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-});
-
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel();
 });
