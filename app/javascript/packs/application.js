0607ceeac6a6c65240f4eb097265b5b1e15e1ba5
Owen Moss <owenkmoss@gmail.com>
2021-05-05 10:19:34 +0200

    homepage js not working and remove js individual files (and keep compressed.js instead)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,43 +21,9 @@ ActiveStorage.start()
 // External imports
 import "bootstrap";
 
-require('./vendor/affix')
-require('./vendor/bootstrap-progressbar.min')
-require('./vendor/bootstrap.bundle')
-require('./vendor/html5shiv.min.js')
-require('./vendor/jflickrfeed.min')
-require('./vendor/jquery-3.3.1.min')
-require('./vendor/jquery-migrate-3.0.0.min')
-require('./vendor/jquery.appear')
-require('./vendor/jquery.cookie')
-require('./vendor/jquery.countdown.min')
-require('./vendor/jquery.countTo')
-require('./vendor/jquery.easing.1.3')
-require('./vendor/jquery.easypiechart.min')
-require('./vendor/jquery.flexslider-min')
-require('./vendor/jquery.hoverIntent')
-require('./vendor/jquery.localScroll.min')
-require('./vendor/jquery.parallax-1.1.3')
-require('./vendor/jquery.prettyPhoto')
-require('./vendor/jquery.scrollbar.min')
-require('./vendor/jquery.scrollTo.min')
-require('./vendor/jquery.ui.totop')
-// require('./vendor/modernizr-2.6.2.min')
-require('./vendor/isotope.pkgd.min')
-require('./vendor/owl.carousel.min')
-require('./vendor/photoswipe')
-// require('./vendor/photoswipe-ui.default.min')
-// require('./vendor/respond.min')
-require('./vendor/spectragram.min')
-require('./vendor/superfish')
-require('./bootstrap.addons')
 require('./compressed')
+require('./bootstrap.addons')
 require('./main')
-
-
-
-
-
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
