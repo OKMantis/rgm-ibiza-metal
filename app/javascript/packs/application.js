9c852f2caa30841d7bc8642d331459cfba60c313
Owen Moss <owenkmoss@gmail.com>
2021-05-06 10:46:35 +0200

    removed requires in applications.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,10 +21,6 @@ ActiveStorage.start()
 // External imports
 import "bootstrap";
 
-require('./compressed')
-require('./bootstrap.addons')
-require('./main')
-
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
