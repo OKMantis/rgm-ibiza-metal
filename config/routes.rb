50eff9c6c60f4635c039891b20337b637f7fe5ce
Owen Moss <owenkmoss@gmail.com>
2021-06-23 19:06:07 +0200

    changed root to home again for staging environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#coming_soon'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'home', to: 'pages#home', as: :home
